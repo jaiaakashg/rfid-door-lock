d200211020bb4f6a27911ea08279dcf398a6a636
jaiaakashg <jaiaakashg28210@gmail.com>
2024-07-04 17:46:43 +0530

    Add files via upload

---
+++ rfid_door_lock2.ino
@@ -0,0 +1,52 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+#define RELAY_PIN 8
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); 
+
+void setup() {
+  Serial.begin(9600); 
+  SPI.begin();       
+  mfrc522.PCD_Init(); 
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH); 
+  Serial.println("Place your card to the reader...");
+}
+
+void loop() {
+  // Look for new cards
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  // Select one of the cards
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+  
+  // Print card UID
+  Serial.print("UID tag :");
+  String content = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  content.toUpperCase();
+  
+  // Check if the UID matches your known card
+  if (content.substring(1) == "03 3B 81 FA") {
+    Serial.println("Access granted");
+    digitalWrite(RELAY_PIN, LOW); 
+    delay(5000); 
+    digitalWrite(RELAY_PIN, HIGH); 
+  } else {
+    Serial.println("Access denied");
+  }
+}
+
+
